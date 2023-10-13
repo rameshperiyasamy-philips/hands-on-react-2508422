13a9cf3a5613473d6869cb081679b34d6d18323a
Ramesh Periyasamy <ramesh.periyasamy@philips.com>
2023-10-13 16:16:03 +0530

    committing 01_01b

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const name= 'StarGazers'
   return (
+    <div className="container">
+      <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="" />
+      <h1>Meet the <i style={{color:"steelblue"}}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <button className="outline" onClick={()=>alert('Hi')}>Click Me</button>
     </hgroup>
+    </article>
+    </div>
   )
 }
 export default App
